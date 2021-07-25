ee88a8181949d1cf64c044c9c60e9ccd7056d169
YaroslavKust <79914649+YaroslavKust@users.noreply.github.com>
2021-07-25 19:23:42 +0300

    full functiona

--- TaskThree/ViewModels/MainWindowViewModel.cs
+++ TaskThree/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,11 @@
 ﻿using TaskThree.Models;
 using System.Collections.Generic;
-using Microsoft.Win32;
-using System.IO;
-using System.Text;
 using System.Linq;
 using System;
 using TaskThree.Export;
+using System.Threading.Tasks;
+using TaskThree.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace TaskThree.ViewModels
 {
@@ -13,15 +13,35 @@ namespace TaskThree.ViewModels
     {
         private TaskThreeContext _db;
         private RelayCommand _loadDataCommand, _exportToXMLCommand, _exportToExcelCommand;
+        private IFileService _file;
+        private IDialogService _dialog;
 
         public List<Record> Records { get; set; }
         public Record FilterRecord { get; set; }
         
-        public MainWindowViewModel()
+        public MainWindowViewModel(IFileService file, IDialogService dialog)
         {
+            _file = file;
+            _dialog = dialog;
             _db = new TaskThreeContext();
-            Records = _db.Records.ToList();
             FilterRecord = new Record();
+        }
+
+        public async Task FilterDataAsync()
+        {
+            Records = await _db.Records.Where(r =>
+                FilterRecord.Date == DateTime.MinValue || r.Date == FilterRecord.Date
+                    &&
+                    String.IsNullOrWhiteSpace(FilterRecord.FirstName) || r.FirstName == FilterRecord.FirstName
+                    &&
+                    String.IsNullOrWhiteSpace(FilterRecord.LastName) || r.LastName == FilterRecord.LastName
+                    &&
+                    String.IsNullOrWhiteSpace(FilterRecord.SurName) || r.SurName == FilterRecord.SurName
+                    &&
+                    String.IsNullOrWhiteSpace(FilterRecord.City) || r.City == FilterRecord.City
+                    &&
+                    String.IsNullOrWhiteSpace(FilterRecord.Country) || r.Country == FilterRecord.Country
+            ).ToListAsync();
         }
 
         public RelayCommand LoadDataCommand
@@ -31,34 +51,13 @@ namespace TaskThree.ViewModels
                 return _loadDataCommand ??
                     (_loadDataCommand = new RelayCommand(obj =>
                     {
-                        string fname;
-                        OpenFileDialog fileDialog = new OpenFileDialog();
+                        string fname = _dialog.OpenDialog();
 
-                        if (fileDialog.ShowDialog() == true)
+                        if (fname != null)
                         {
-                            fname = fileDialog.FileName;
-                            StreamReader reader = new StreamReader(fname, Encoding.UTF8);
-
-                            string line;
-                            while((line = reader.ReadLine()) != null)
-                            {
-                                string[] data = line.Split(';');
-                                Record rec = new Record()
-                                {
-                                    Date = DateTime.Parse(data[0]),
-                                    FirstName = data[1],
-                                    LastName = data[2],
-                                    SurName = data[3],
-                                    City = data[4],
-                                    Country = data[5]
-                                };
-
-                                _db.Add(rec);
-                                _db.SaveChanges();
-                            }
-
+                            var results = _file.ReadAll(fname);
+                            _db.AddRangeAsync(results);
                         }
-
                     }));
             }
         }
@@ -70,7 +69,7 @@ namespace TaskThree.ViewModels
                 return _exportToXMLCommand ??
                     (_exportToXMLCommand = new RelayCommand(obj => 
                     {
-                        ExportToFormat(new XMLExporter());
+                        ExportToFormatAsync(new XMLExporter());
                     }));
             }
         }
@@ -82,14 +81,18 @@ namespace TaskThree.ViewModels
                 return _exportToExcelCommand ??
                     (_exportToExcelCommand = new RelayCommand(obj =>
                     {
-                        ExportToFormat(new ExcelExporter());
+                        ExportToFormatAsync(new ExcelExporter());
                     }));
             }
         }
 
-        private void ExportToFormat(IExporter exp)
+        private async void ExportToFormatAsync(IExporter exp)
         {
-            exp.Export(Records);
+            await FilterDataAsync();
+            await Task.Run(() =>
+            {
+                exp.Export(Records, _dialog.SaveDialog());
+            });
         }
     }
 }
